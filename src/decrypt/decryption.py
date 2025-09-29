e9769d6311195185f810f25520c79ef14fa89374
Ethan Hopkins <ethan.e.hopkins@gmail.com>
2025-09-29 08:44:30 -0700

    working decrypt (should be updated to be more efficient in the future though)

--- src/decrypt/decryption.py
+++ src/decrypt/decryption.py
@@ -4,62 +4,30 @@ import numpy as np
 # from collections import defaultdict
 # import matplotlib.pyplot as plt
 
-
 def decrypt():
     with open("data/cipher_0_dir/priv_0.txt", "r") as file:
         priv = file.read()
+        print("PRIV", priv)
+
+        def assign(x):
+            return int(priv[int(x-2)])
+        v_assign = np.vectorize(assign)
 
         with h5py.File("data/cipher_0_dir/cipher_0.hdf5", "r") as file:
             if "expression" in file:
-                expression = file["expression"]
+                expression = file.get("expression")
+                expression = np.array(expression[:]) 
+                
+                expression = [all(v_assign(term)) for term in expression]
+                expression = filter(lambda term: term, expression)
+                expression = list(expression)
 
-                # lengths = defaultdict(int)
+                size = sum(1 for _ in expression)
+                
+                g = size % 2
 
-                # for i in expression:
-                #     l = len(i)
-                #     lengths[l] += 1
-
-                # plt.bar(lengths.keys(), lengths.values())
-                # plt.xlabel('monomial term sizes')
-                # plt.ylabel('count')
-                # plt.show()
-
-                # for term in expression:
-                # parity = int(all(list(map(lambda literal: int(priv[literal-4]), term))))
-
-                # g = g^parity
-
-                g = 1
-
-                for term in expression:
-                    term = np.subtract(term, 2).astype(int)
-
-                    term_parity = int(
-                        all(map(lambda literal: int(priv[literal-1]), term))
-                    )
-
-                    g = g ^ term_parity
-                    # print(g)
-
-                    # if g == term_parity:
-                    #     print(not g)
-
-                    # g = g^term_parity
-
-                    # if g_updated^g:
-                    #     print(g_updated)
-
-                    # g = g_updated
-                    # 1^1 = 0
-                    # 1^0 = 1
-                    # 0^1 = 1
-                    # 0^0 = 0
-
-                    # if not g^parity:
-                    #     print()
-                    # g = g^parity
-
-                print(g)
+                return g
 
 
-decrypt()
+
+print(decrypt())
